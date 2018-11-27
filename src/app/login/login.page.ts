4b71adf6f4cc49d5dc9bb7bab29fd9bedbc427a0
Milind Rohokale <mirohoka@igategegdc.com>
2018-11-27 22:30:13 +0530

    fifth commit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  username: String;
+  password: String;
+  constructor(public toastController: ToastController,
+    private router: Router) { }
 
   ngOnInit() {
   }
+  login() {
+    if (this.username === 'admin' && this.password === 'admin') {
+      this.presentToastWithOptions('Successfully logged in');
+      this.router.navigate(['/map']);
+    } 
+    else if (this.username === 'worker' && this.password === 'worker') {
+      this.presentToastWithOptions('Successfully logged in');
+      this.router.navigate(['/map']);
+    } 
+    else {
+      this.presentToastWithOptions('Invalid credentials');
+      this.router.navigate(['/login']);
+    }
+  }
 
+  async presentToastWithOptions(text) {
+    const toast = await this.toastController.create({
+      message: text,
+      showCloseButton: true,
+      position: 'bottom',
+      closeButtonText: 'Done',
+      duration: 1000
+    });
+    toast.present();
+  }
 }
